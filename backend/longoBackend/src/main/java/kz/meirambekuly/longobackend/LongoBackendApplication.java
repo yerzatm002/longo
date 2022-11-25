2d5923e0b00a8333d8168844231750a14fda2823
yerzat <erzatnis@gmail.com>
2022-11-25 09:37:29 +0600

    first commit

---
+++ backend/longoBackend/src/main/java/kz/meirambekuly/longobackend/LongoBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.meirambekuly.longobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongoBackendApplication.class, args);
+    }
+
+}
---
+++ backend/longoBackend/src/test/java/kz/meirambekuly/longobackend/LongoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.meirambekuly.longobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
